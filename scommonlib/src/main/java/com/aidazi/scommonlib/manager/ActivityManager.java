64b7160051fd23325435311ca861c1279a3b346c
yuan_qing <ttaidazi@gmail.com>
2019-11-07 18:20:16 +0800

    修改Activitymanager的bug

--- scommonlib/src/main/java/com/aidazi/scommonlib/manager/ActivityManager.java
+++ scommonlib/src/main/java/com/aidazi/scommonlib/manager/ActivityManager.java
@@ -32,12 +32,18 @@ public class ActivityManager {
 
 
     public void removeActivity(Activity activity) {
+        if (activityStack == null || activityStack.size() == 0) {
+            return;
+        }
         if (activity != null) {
             activityStack.remove(activity);
         }
     }
 
     public void finishActivity(Class<?> cls) {
+        if (activityStack == null || activityStack.size() == 0) {
+            return;
+        }
         Iterator var2 = activityStack.iterator();
         Activity activity;
         do {
@@ -50,6 +56,9 @@ public class ActivityManager {
     }
 
     public void finishAllActivity() {
+        if (activityStack == null || activityStack.size() == 0) {
+            return;
+        }
         int i = 0;
         for (int size = activityStack.size(); i < size; ++i) {
             if (null != activityStack.get(i)) {
